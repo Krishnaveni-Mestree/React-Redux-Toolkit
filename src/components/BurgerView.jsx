0429615342e6766660ceaea8d67a44c42002c6e9
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-11 15:30:55 +0530

    we can read the state from store into our components by using useSelector hook

--- src/components/BurgerView.jsx
+++ src/components/BurgerView.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function BurgerView() {
+    const burgerBuns=useSelector((state)=>state.burger.burgerBuns);
+    //console.log(burgerBuns);
   return (
     <div>
-      <h2>Number of Burger buns - 50</h2>
+      <h2>Number of Burger buns - {burgerBuns}</h2>
       <button>Order Burger</button>
     </div>
   )
--- src/components/PizzaView.jsx
+++ src/components/PizzaView.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function PizzaView() {
+    const pizzaBase=useSelector((state)=>state.pizza.pizzaBase);
+    console.log(pizzaBase)
   return (
     <div>
-      <h2>Number of Pizza buns - 1000</h2>
+      <h2>Number of Pizza buns - {pizzaBase}</h2>
       <button>Order Pizza</button>
     </div>
   )
