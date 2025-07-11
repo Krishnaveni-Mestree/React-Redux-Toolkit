69cb53af07c96bad405603d305a884ac1dd821bd
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-11 15:42:48 +0530

    we r updating by accessing disptach function with help of useDispatch hook

--- src/components/PizzaView.jsx
+++ src/components/PizzaView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { pizza_order } from '../features/pizza/pizzaSlice';
 
 function PizzaView() {
     const pizzaBase=useSelector((state)=>state.pizza.pizzaBase);
-    console.log(pizzaBase)
+    const dispatch=useDispatch();
+    console.log(dispatch);
   return (
     <div>
       <h2>Number of Pizza buns - {pizzaBase}</h2>
-      <button>Order Pizza</button>
+      <button onClick={()=>dispatch(pizza_order())}>Order Pizza</button>
     </div>
   )
 }
