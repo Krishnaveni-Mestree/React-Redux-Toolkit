64dbddcc75ba931dbd1a88e20a1a6a85382b6b88
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-11 14:44:15 +0530

    created components for rendering

---
+++ src/components/BurgerView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BurgerView() {
+  return (
+    <div>
+      <h2>Number of Burger buns - 50</h2>
+      <button>Order Burger</button>
+    </div>
+  )
+}
+
+export default BurgerView;
---
+++ src/components/PizzaView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PizzaView() {
+  return (
+    <div>
+      <h2>Number of Pizza buns - 1000</h2>
+      <button>Order Pizza</button>
+    </div>
+  )
+}
+
+export default PizzaView;
