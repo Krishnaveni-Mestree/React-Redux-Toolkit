60a029f5109452c7902c60bbef7d3727a975dcb2
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-11 15:11:10 +0530

    we have provided store to access our app components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
+
+
+//we have to give PizzaView and BurgerView components access to my redux store
+//we can give access to the store by using Provider component
+//Provider Component from React-Redux -->official UI binding for react
